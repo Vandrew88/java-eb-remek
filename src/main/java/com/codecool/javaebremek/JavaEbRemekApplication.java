fb1fc9fb896dfe6065d1b96026f35307c4f5f497
Vandrew88 <andras.vasmatics@gmail.com>
2021-08-09 17:58:53 +0200

    Add as springBoot project

---
+++ src/main/java/com/codecool/javaebremek/JavaEbRemekApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.javaebremek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEbRemekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEbRemekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/javaebremek/JavaEbRemekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.javaebremek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEbRemekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
