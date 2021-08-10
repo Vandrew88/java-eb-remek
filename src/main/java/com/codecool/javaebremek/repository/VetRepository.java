e813e5a45883245bb34bd3fcdc7b2f4d5919ebd0
Vandrew88 <andras.vasmatics@gmail.com>
2021-08-10 17:14:00 +0200

    Add repositories

---
+++ src/main/java/com/codecool/javaebremek/repository/VetRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.javaebremek.repository;
+
+import com.codecool.javaebremek.model.Vet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VetRepository extends JpaRepository<Vet, Long> {
+}
